0106aa84115de8c8efcec36248f5ac8cca9311d8
Avsoft_AUR <avsoft.studio@outlook.fr>
2020-08-31 15:33:45 +0200

    Ajout du projet

---
+++ js/test.js
@@ -0,0 +1,4 @@
+"use strict";
+var fs = require('fs');
+var monJson = JSON.parse('./data/document/test.json');
+console.log(monJson);
---
+++ ts/test.ts
@@ -0,0 +1,4 @@
+var fs = require('fs');
+var monJson = JSON.parse('./data/document/test.json');
+
+console.log(monJson);
